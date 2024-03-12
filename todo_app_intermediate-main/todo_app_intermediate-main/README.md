e0acdda0173175afbe3a07a19f515ac998299e13
Vishal Nanaware <nanawarevishal18@gmail.com>
2024-03-12 22:14:42 +0530

    TODO App

--- todo_app_intermediate-main/todo_app_intermediate-main/README.md
+++
@@ -1,3 +0,0 @@
-# todo_app_intermediate
-
-Todo App with backend with sqflite.
--- todo_app_intermediate-main/todo_app_intermediate-main/android/app/src/main/kotlin/com/example/todo_app_intermediate/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.todo_app_intermediate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
